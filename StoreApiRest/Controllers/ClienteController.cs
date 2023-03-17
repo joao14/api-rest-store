db55ed34e101cbf2be616ddf833a2203beced10f
Alexander Merino <barcealex1014@hotmail.com>
2023-03-16 22:11:40 -0500

    feat: add first structure of api rest

---
+++ StoreApiRest/Controllers/ClienteController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using StoreApiRest.Models;
+
+namespace StoreApiRest.Controllers
+{
+    [ApiController]
+    [Route("client")]
+    public class ClienteController: ControllerBase
+	{
+
+        [HttpGet]
+        [Route("list")]
+        public dynamic getClients()
+        {
+            List<Client> clients = new List<Client>
+            {
+                new Client
+                {
+                    id= "1",
+                    correo="alexmerino67@gmail.com",
+                    edad="20",
+                    nombre="Alexander Merino"
+                },
+
+                new Client
+                {
+                    id= "2",
+                    correo="barcealex1014@hotmail.com",
+                    edad="25",
+                    nombre="Miguel Merino"
+                }
+            };
+
+            return clients;
+        }
+
+        //[HttpPost]
+        //[Route("add")]
+        //public dynamic addClient()
+        //{
+
+        //}
+
+    }
+}
+
---
+++ StoreApiRest/Models/Client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace StoreApiRest.Models
+{
+	public class Client
+	{
+        public string id { get; set; }
+        public string nombre { get; set; }
+		public string edad { get; set; }
+        public string correo { get; set; }
+    }
+}
+
