c257f5ccf68a91bc952eea4201022010dd92e10e
Alexander Merino <barcealex1014@hotmail.com>
2023-03-17 21:37:35 -0500

    feat: crud entity client

---
+++ StoreApiRest/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using StoreApiRest.Models;
+
+namespace StoreApiRest.Datos
+{
+	public class ApplicationDbContext: DbContext
+	{
+		public DbSet<Client> Clients { get; set; }
+	}
+}
+
